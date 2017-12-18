e416deec488fc1b928b7c7722495d08399471e88
Joshua Wolfe <jlwolfe1990@gmail.com>
2017-12-18 00:33:46 -0600

    Fixup transaction sync and report issue

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,6 @@
 class Transaction < ApplicationRecord
+  default_scope { order(date: :desc) }
+
   belongs_to :classification, optional: true
   belongs_to :rule, optional: true
   belongs_to :account
@@ -9,6 +11,7 @@ class Transaction < ApplicationRecord
   scope :for_user, -> (user) { joins(:account).where(accounts: { user_id: user.id }) }
   scope :focus, -> { where("transactions.classification_id NOT IN (#{Classification.ignored.map(&:id).join(', ')}) or transactions.classification_id IS NULL") }
   scope :range, ->(start_at, end_at) { where('date >= ? and date <= ?', start_at.strftime('%Y/%m/%d'), end_at.strftime('%Y/%m/%d'))}
+
 
   def pending?
     !!plaid_pending
